143f19102ccc2e4dae819062eefd431fc0ef31c1
xRadne <Alex.radne@gmail.com>
2019-11-22 10:58:44 +0100

    Example Commitment

--- InitializePopulation.m
+++ InitializePopulation.m
@@ -1,6 +1,6 @@
 function newPopulation = InitializePopulation(nGenes, populationSize)
 %INITIALIZEPOPULATION Summary of this function goes here
 %   Detailed explanation goes here
-    newPopulation = rand(populationSize, nGenes);
+    newPopulation = rand(populationSize, nGenes) + 2;
 end
 
