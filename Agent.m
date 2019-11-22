421ce623dee0613c867f8d5b4a561741b829c031
xRadne <Alex.radne@gmail.com>
2019-11-22 11:48:54 +0100

    Implemented Agent Class and Initialize Population

---
+++ Agent.m
@@ -0,0 +1,21 @@
+classdef Agent
+    %AGENT Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        x
+        y
+        chromosome
+    end
+    
+    methods
+        function obj = Agent(chromosome, gridSize)
+            %AGENT Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.x = ceil(rand() * gridSize);
+            obj.y = ceil(rand() * gridSize);
+            obj.chromosome = chromosome;
+        end
+    end
+end
+
--- InitializePopulation.m
+++ InitializePopulation.m
@@ -1,6 +1,11 @@
 function newPopulation = InitializePopulation(nGenes, populationSize)
 %INITIALIZEPOPULATION Summary of this function goes here
 %   Detailed explanation goes here
-    newPopulation = rand(populationSize, nGenes) + 2;
+    array = repmat(Agent(0, 0),1, populationSize); % Preallocation
+    for i = 1: populationSize
+        randomChromosome = rand(1, nGenes);
+        array(i) = Agent(randomChromosome, 100);
+    end
+    newPopulation =  array;
 end
 
