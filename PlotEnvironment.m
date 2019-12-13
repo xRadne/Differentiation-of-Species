fa0cd5af027a8838cf58030268e41bfcee3289c8
xRadne <Alex.radne@gmail.com>
2019-12-13 16:30:10 +0100

    Clear plot between iterations

--- PlotEnvironment.m
+++ PlotEnvironment.m
@@ -1,7 +1,7 @@
 function [] = PlotEnvironment(agentX,agentY,foodX,foodY)
     pause(0.01);
-    hold on
     plot(foodX,foodY,'or');
+    hold on
     plot(agentX,agentY,'.k');
     hold off
     drawnow
