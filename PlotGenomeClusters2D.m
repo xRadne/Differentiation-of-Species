b45b55d4cd12be90a95b9a59b4fdff6bd07f949c
xRadne <Alex.radne@gmail.com>
2019-12-13 12:39:37 +0100

    Golden Ratio Spread of the Color hue values

--- PlotGenomeClusters2D.m
+++ PlotGenomeClusters2D.m
@@ -1,12 +1,12 @@
 function [] = PlotGenomeClusters2D(gene1, gene2, components)
 %PLOTGENOMECLUSTERS2D Components is a vector of integers specifying what
 %specie/cluster/component an individual agent belongs to.
-    
-
+    phi = 1/2+sqrt(5)/2;
 
     % Plot Positions/Genes
     nComponents = max(components);
-    hue = linspace(0,1-1/nComponents,nComponents)'*[1 0 0]; % Column vector with equally spaced hues
+    goldenRatioSpread = mod(phi*(1:nComponents), 1);
+    hue = goldenRatioSpread'*[1 0 0]; % Column vector with equally spaced hues
     hsvColor = hue + [0 1 1]; % Set saturation and brightness to 1 for all colors
     rgbColor = hsv2rgb(hsvColor); % Convert to RGB values
 
