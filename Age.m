4a37d46427e1a772c5f7e1ca72cb32eb4d7b585a
ellensanden <53423025+ellensanden@users.noreply.github.com>
2019-12-13 13:51:33 +0100

    vectorized Age and Mutate

--- Age.m
+++ Age.m
@@ -1,26 +1,14 @@
-function newpopulation=Age(population,maxLife)
-%make agents die after a certain amount of time, later on affected by how much they eat
+function [aliveAge,aliveX,aliveY,aliveChromosome]=Age(agentX,agentY,agentChromosome,agentAge,maxLife)
 
-% energy function rather than time function?
-% make age function probabilistic with the higher age, the higher the risk
-% of dying
-    populationSize = length(population);
-    deathRow=[];
+% age function. the higher age, the higher the risk of dying
+ 
+     agentAge=agentAge+1; 
+
+     deathParameter=randi([1,maxLife],1,length(agentAge));
+     aliveIdx=find(agentAge < deathParameter);
+     aliveAge=agentAge(aliveIdx);
+     aliveX=agentX(aliveIdx);
+     aliveY=agentY(aliveIdx);
+     aliveChromosome=agentChromosome(:,aliveIdx);
     
- for i=1:length(population)
-     deathParameter=randi([1,maxLife],1);
-     population(1,i).age=population(1,i).age + 1;
-     alive(i)=(deathParameter > population(1,i).age);
-
-    % alive(i)=population(randi([1,maxLife],populationSize) > population(1,:).age);
-     %deathRow(i)=(population(1,i).age==tDeath);
- end
-%  deathRow=find(deathRow);
-%  if not(isempty(deathRow))
-%  population(deathRow)=[];
-%  end
-%population(:,population(1,:).age == tDeath) =[]; should be able to make it
-%faster without loop
-%newpopulation=population;
-newpopulation=population(alive);
  end
