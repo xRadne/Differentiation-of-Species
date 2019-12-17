00603479b976b97641eada15383c94569a36ee0c
juliawennerblom <julia.wennerblom@hotmail.com>
2019-12-17 10:36:54 +0100

    Updated graphics

--- PlotEnvironment.m
+++ PlotEnvironment.m
@@ -1,17 +1,23 @@
 function [] = PlotEnvironment(agentX,agentY,foodX,foodY,agentChromosome,foodAmount)
-    %pause(0.01);
-    %plot(foodX,foodY,'or');
-    
-   % map=[zeros(1,length(agentX))',zeros(1,length(agentX))',linspace(1,0.001,length(agentX))'];
-    %colormap(map)
-    sizeFood=foodAmount.*200;
-    scatter(foodX,foodY,sizeFood,'r','filled')
-    hold on
+%pause(0.01);
+%plot(foodX,foodY,'or');
 
-    sizeIndividual=agentChromosome(1,:).*100;
-    %plot(agentX,agentY,'.k');
-    %plot(agentX,agentY,'o','MarkerSize',Size)
-    scatter(agentX,agentY,sizeIndividual)%, sizeIndividual)
-    hold off
-    drawnow
+% map=[zeros(1,length(agentX))',zeros(1,length(agentX))',linspace(1,0.001,length(agentX))'];
+%colormap(map)
+sizeFood=foodAmount.*200;
+scatter(foodX,foodY,sizeFood,'r','filled')
+hold on
+
+sizeIndividual=agentChromosome(1,:).*100;
+%plot(agentX,agentY,'.k');
+%plot(agentX,agentY,'o','MarkerSize',Size)
+scatter(agentX,agentY,sizeIndividual)%, sizeIndividual)
+axis tight;
+axis([0 100 0 100]);
+hold on
+I = imread('KondyorMassif3.png');
+h = image(xlim,ylim,I);
+uistack(h,'bottom')
+hold off
+drawnow
 end
