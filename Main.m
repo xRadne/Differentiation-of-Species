81cb6f4760519ebc17589ba81a8d402ebf3d1737
juliawennerblom <julia.wennerblom@hotmail.com>
2019-12-18 17:04:25 +0100

    Updated parameters

--- Main.m
+++ Main.m
@@ -7,26 +7,26 @@
 
 clear;
 nGenes = 2;
-initialPopulationSize = 10; 
+initialPopulationSize = 15; 
 gridSize = 100;
 maxFood = 1;
 biteSize = 0.1;
-foodRegenerateAmount = 0.01;
+foodRegenerateAmount = 0.08;
 
 lowerX = 40/100*gridSize;
 midX = 50/100*gridSize;
 higherX = 75/100*gridSize;
 lowerY = 30/100*gridSize;
 higherY = 68/100*gridSize;
-nFood = 100;
+nFood = 60;
 foodRadius = 5;
 foodEdabilityRange = 0.3;
 distanceParameter = 0.3;
 matingProbability = 1.0; % set to reasonable value
-mutationProbability=0.2; % set to reasonable value
-mutationParameter=0.05; % set to reasonable value
+mutationProbability=0.5; % set to reasonable value
+mutationParameter=0.2; % set to reasonable value
 matingDistance=0.5; % step size in walk function??
-sightParameter=50;
+sightParameter=20;
 mMin=0.0001; % set to reasonable value
 mMax=0.9999; % set to reasonable value
 maxLife=100;
@@ -35,19 +35,23 @@ hungerParameter=0.01;
 maxHunger=10;
 goingRadius = sightParameter;
 mateRadius = matingDistance;
+speedParameter=4;
 
 % INITIALIZE POPULATION
 time = 0;
 agentX=randi([0, gridSize],1,initialPopulationSize); 
 agentY=randi([0, gridSize],1,initialPopulationSize);
-agentChromosome=rand(nGenes,initialPopulationSize); 
+%agentChromosome=rand(nGenes,initialPopulationSize); 
+agentChromosome=zeros(nGenes,initialPopulationSize)+0.5; 
 agentAge=zeros(1,initialPopulationSize);
 agentHunger=ones(1,initialPopulationSize);
 geneticDistance = GeneticDistance(agentChromosome); 
 sightRadius = sightParameter * ones(1,initialPopulationSize);
-speed=(1-agentChromosome(1,:))*2;
+speed=(1-agentChromosome(1,:))*speedParameter;
 nAgents=initialPopulationSize;
 geneticDistanceParameter = distanceParameter;
+populationSizeVector=[];
+
 
 
 % INITIALIZE FOOD
@@ -72,16 +76,28 @@ while nAgents>0
     [agentHunger,foodAmount,foodX,foodY,iClosestFood,squaredDistanceClosestFood] = Eat(agentX,agentY,agentHunger,foodX,foodY,foodAmount,foodRadius,foodType,foodEdabilityRange,biteSize,agentChromosome,gridSize);
  
     geneticDistance = GeneticDistance(agentChromosome); 
-    speed=(1-agentChromosome(1,:))*2;
+    speed=(1-agentChromosome(1,:))*speedParameter;
     
     iClosestEligableMate = ClosestEligableMate(agentX,agentY,agentChromosome,geneticDistanceParameter);
     [agentX,agentY] = ValleyWalk(agentX,agentY,speed,sightRadius,foodX,foodY,foodAmount,agentHunger,maxHunger,gridSize,lowerX,midX,higherX,lowerY,higherY,iClosestFood,squaredDistanceClosestFood,iClosestEligableMate,goingRadius,mateRadius,biteSize);
     
-    PlotEnvironment(agentX,agentY,foodX(foodAmount>biteSize),foodY(foodAmount>biteSize),agentChromosome,foodAmount(foodAmount>biteSize),lowerX,midX,higherX,lowerY,higherY,gridSize,foodType(foodAmount>biteSize));
+    %PlotEnvironment(agentX,agentY,foodX(foodAmount>biteSize),foodY(foodAmount>biteSize),agentChromosome,foodAmount(foodAmount>biteSize),lowerX,midX,higherX,lowerY,higherY,gridSize,foodType(foodAmount>biteSize));
     nAgents=size(agentX,2);
     
     time = time + 1; % Timestep done
+    populationSizeVector(1,time)=nAgents;
     frames(time) = getframe(fig);
+    
+    if(mod(time,100)==0)
+        mutationProbability=mutationProbability*0.9; % set to reasonable value
+        mutationParameter=mutationParameter*0.9;
+        if(mutationParameter<0.05)
+            mutationParameter=0.05;
+        end
+        if(mutationParameter<0.2)
+            mutationParameter=0.2;
+        end
+    end
 end
 
 %% SAVE VIDEO
@@ -116,3 +132,10 @@ close(writerObj);
 figure(1); clf;
 species = ComputeComponents(agentChromosome,distanceParameter);
 PlotGenomeClusters2D(agentChromosome(1,:),agentChromosome(2,:),species);
+
+%% POPULATION DEVELOPMENT
+clf
+
+nTimeSteps=size(populationSizeVector,2);
+timeVector=linspace(1,nTimeSteps,nTimeSteps);
+plot(timeVector,populationSizeVector)
